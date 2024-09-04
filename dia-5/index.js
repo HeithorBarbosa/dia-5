e3d04a7a021b5e517ab161f36a19c3f4978fec18
Heithor Barbosa <heithor.barbosa321@gmail.com>
2024-09-04 10:48:51 -0300

    dia-5 ex1

--- dia-5/index.js
+++ dia-5/index.js
@@ -1,13 +1,23 @@
-let idade = Number(prompt("você tem 17 ou 18 ?"));
+let num1 = Number(prompt("digite um número"));
+let num2 = Number(prompt("digite outro número"));
+let operation = prompt(
+  "qual operação voçê gostaria de fazer ?" + "\n1 = + \n2 = - \n3 = / \n4 = *"
+);
 
-switch (idade) {
-  case 17:
-    document.write("você tem 17");
+switch (operation) {
+  case "1":
+    console.log(num1 + num2);
     break;
-  case 18:
-    document.write("você tem 18");
+  case "2":
+    console.log(num1 - num2);
+    break;
+  case "3":
+    console.log(num1 / num2);
+    break;
+  case "4":
+    console.log(num1 * num2);
     break;
   default:
-    document.write("você não tem nenhuma dessas idades");
+    console.log("talvez você escreveu errado a operação");
     break;
 }
